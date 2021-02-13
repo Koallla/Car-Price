74c8edf3ed44ea4926c5c522353e944bf2f485ec
Mihail <41691809+Koallla@users.noreply.github.com>
2021-02-13 19:43:26 +0200

    added pypi package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='car_price',
-    version='1.0',
+    version='1.0.1',
     description='car cost calculation',
     url='https://github.com/Koallla/goit-python/tree/main/lesson7/clean_folder',
     author='Mihail Zmiiov',
     author_email='zmiyov2422@gmail.com',
-    license='FREE',
+    license='MIT',
     packages=find_namespace_packages(),
     entry_points={'console_scripts': ['car = car_price.price_car:tax_auction']})
